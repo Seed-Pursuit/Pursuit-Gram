dbe09064b54ab4747712ec8607e39d2c82b0f271
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-12-23 15:10:53 +0530

    Add button component and update paths

--- pursuitgram/src/App.tsx
+++ pursuitgram/src/App.tsx
@@ -3,17 +3,29 @@ import './globals.css'
 import { Route, Routes } from 'react-router-dom'
 import { Home } from './_root/pages'
 import SigninForm from './_auth/forms/SigninForm'
+import SignupForm from './_auth/forms/SignupForm'
+import AuthLayout from './_auth/AuthLayout'
+import RootLayout from './_root/RootLayout'
 
 const App = () => {
   return (
-   <main className='flex h-screen'>
-    <Routes>
+    <main className='flex h-screen'>
+      <Routes>
         {/* public route */}
-        <Route path='/sign-in' element={<SigninForm/>}/>
+        <Route element={<AuthLayout />}>
+          <Route path='/sign-in' element={<SigninForm />} />
+          <Route path='/sign-up' element={<SignupForm />} />
+
+        </Route>
+
         {/* private route */}
-        <Route index element={<Home/>}/>
-    </Routes>
-   </main>
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+          
+        </Route>
+
+      </Routes>
+    </main>
   )
 }
 
--- pursuitgram/src/_auth/AuthLayout.tsx
+++ pursuitgram/src/_auth/AuthLayout.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { Outlet, Navigate } from "react-router-dom"
 
 const AuthLayout = () => {
+  const isAuthenticated = false;
   return (
-    <div>AuthLayout</div>
+    <>
+      {
+        isAuthenticated ? (
+          <Navigate to='/' />
+        ) : (
+          <>
+            <section className="flex flex-col justify-center items-center w-full h-screen">
+              <Outlet />
+            </section>
+
+            <img
+              src="images/side-img.png"
+              className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+              alt="logo"
+            />
+          </>
+        )
+      }
+
+    </>
   )
 }
 
