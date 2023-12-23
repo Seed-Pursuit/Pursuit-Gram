3410b84b45d47aa0622e7acdda12e35012558a55
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-12-23 13:57:45 +0530

    Add tailwindcss-animate dependency and update styles

--- pursuitgram/src/App.tsx
+++ pursuitgram/src/App.tsx
@@ -1,10 +1,11 @@
 import './main.css'
+import './globals.css'
 
 const App = () => {
   return (
     <div>
         <h1 className='text-3x1 font-bold underline'>
-            hello world
+            Hello PursuitGram
         </h1>
     </div>
   )
