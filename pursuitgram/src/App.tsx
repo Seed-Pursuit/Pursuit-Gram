223d7f5702098fb08497c8e829632c4e71b6578b
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-12-23 14:07:09 +0530

    Add react-router-dom dependency and update routing in App component

--- pursuitgram/src/App.tsx
+++ pursuitgram/src/App.tsx
@@ -1,13 +1,19 @@
 import './main.css'
 import './globals.css'
+import { Route, Routes } from 'react-router-dom'
+import { Home } from './_root/pages'
+import SigninForm from './_auth/forms/SigninForm'
 
 const App = () => {
   return (
-    <div>
-        <h1 className='text-3x1 font-bold underline'>
-            Hello PursuitGram
-        </h1>
-    </div>
+   <main className='flex h-screen'>
+    <Routes>
+        {/* public route */}
+        <Route path='/sign-in' element={<SigninForm/>}/>
+        {/* private route */}
+        <Route index element={<Home/>}/>
+    </Routes>
+   </main>
   )
 }
 
